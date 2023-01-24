e75f0bfbce9ea862f3a2fc5992bb3d2d77ec8a41
дом <amir/shakirov.2015@mail.ru>
2023-01-24 22:01:42 +0400

    Initial commit

---
+++ src/main/java/com/example/hwcoursework3web/HwCourseWork3WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwcoursework3web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCourseWork3WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwCourseWork3WebApplication.class, args);
+    }
+
+}
